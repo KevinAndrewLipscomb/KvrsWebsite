b1624531ca28ca23c0799921a87da1dfa78cd481
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2012-06-16 22:55:51 +0000

    Removed command trying to open an FTP session to Alentus.

--- work.bat
+++ work.bat
@@ -3,6 +3,5 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\KvrsWebsite"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KvrsWebsite\.svn
-start /max explorer ftp://kalipso:@sslws038.alentus.com/ssl/KvrsWebsite_x
 start /max KvrsWebsite.sln
 start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
