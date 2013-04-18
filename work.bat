98ea573a6c22252689df959f8792d5fee365b8ef
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2013-04-18 20:59:25 +0000

    Upgraded MySql.Data.dll.  Revised 1-user-setup.sql files to account for a peculiarity involving commands following full comment lines.  Added missing timeout.aspx* files to solutions.  Updated/standardized timeout.aspx* files where appropriate.  Made the MySQL Workbench call in the work.bat files more specific because the more general version that depended on junction points was causing a "blocked DLL" problem.  Recompiled with latest version of ki.dll and, where appropriate, emsi.dll.

--- work.bat
+++ work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\KvrsWebsite"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KvrsWebsite\.svn
 start /max KvrsWebsite.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench CE 5.2.47\MySQLWorkbench.exe"
