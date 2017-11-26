a7cc18f82cc10f6a017c6b8dfabedbe0c09cbfec
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2017-11-26 04:03:27 +0000

    Converted News section of Default page to syndicate the KVRS blog from Blogger.

--- trunk/kvrs/website/Default.aspx.cs
+++ trunk/kvrs/website/Default.aspx.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Configuration;
+using System.ServiceModel.Syndication;
+using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace Default
-{
-    public partial class TWebForm_Default: ki_web_ui.page_class
+  {
+  public partial class TWebForm_Default: ki_web_ui.page_class
     {
         // / <summary>
         // / Required method for Designer support -- do not modify
@@ -14,12 +17,14 @@ namespace Default
             //this.Load += this.Page_Load;
         }
 
-        protected void Page_Load(object sender, System.EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 Title = Server.HtmlEncode(ConfigurationManager.AppSettings["application_name"]) + " - Default";
                 //Response.Redirect("~/protected/overview.aspx");
+                Repeater_blog.DataSource = SyndicationFeed.Load(XmlReader.Create("https://kvrs9.blogspot.com/feeds/posts/default")).Items;
+                Repeater_blog.DataBind();
             }
         }
 
@@ -29,6 +34,17 @@ namespace Default
             InitializeComponent();
             base.OnInit(e);
         }
+
+    protected void Repeater_blog_ItemDataBound(object sender, RepeaterItemEventArgs e)
+      {
+      if (e.Item.ItemType.ToString().EndsWith("Item"))
+        {
+        var syndication_item = (e.Item.DataItem as SyndicationItem);
+        (e.Item.FindControl("Literal_date") as Literal).Text = syndication_item.Title.Text;
+        (e.Item.FindControl("Literal_content") as Literal).Text = (syndication_item.Content as TextSyndicationContent).Text;
+        (e.Item.FindControl("Literal_author") as Literal).Text = syndication_item.Authors[0].Name;
+        }
+      }
 
     } // end TWebForm_Default
 
