7641c955f9aea994efd24e20bb8b5bd79a7b8083
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2020-05-09 20:42:19 +0000

    Revised to address issues identified by code analysis tool.

--- mark-2/operations.aspx.cs
+++ mark-2/operations.aspx.cs
@@ -11,7 +11,6 @@ namespace operations
         // / </summary>
         private void InitializeComponent()
         {
-            //this.Load += this.Page_Load;
         }
 
         protected void Page_Load(object sender, System.EventArgs e)
