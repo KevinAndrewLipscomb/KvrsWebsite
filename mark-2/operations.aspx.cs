9a9d12acddd48c1a3b7f9a89ad97f3e4352f5a2b
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2022-02-26 00:44:38 +0000

    Corrected to set title element on every page load.

--- mark-2/operations.aspx.cs
+++ mark-2/operations.aspx.cs
@@ -15,9 +15,9 @@ namespace operations
 
         protected void Page_Load(object sender, System.EventArgs e)
         {
+            Title = Server.HtmlEncode(ConfigurationManager.AppSettings["application_name"]) + " - operations";
             if (!IsPostBack)
             {
-                Title = Server.HtmlEncode(ConfigurationManager.AppSettings["application_name"]) + " - operations";
                 //Response.Redirect("~/protected/overview.aspx");
             }
         }
