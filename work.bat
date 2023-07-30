398c93645574e46107ebe4b9138f56e57991bc0d
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2023-07-30 09:26:49 -0400

    Added new ambulance contract. Replaced .svn reference with .git in work.bat.

--- work.bat
+++ work.bat
@@ -2,6 +2,6 @@ REM
 REM $Id: work.bat 5487 2014-11-22 22:50:44Z kevinanlipscomb $
 REM
 cd "C:\Inetpub\wwwroot\KvrsWebsite"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\KvrsWebsite\.svn
+start /max explorer /e,/select,C:\Inetpub\wwwroot\KvrsWebsite\.git
 start /max KvrsWebsite.sln
 IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
